c4983f7527f9972fb01aca39c0ed556d2c9790f0
Ben <doc.ben.johnson@gmail.com>
2020-04-16 16:20:10 -0400

    added a lobby!

--- sayit/views.py
+++ sayit/views.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse, HttpResponseRedirect
@@ -10,11 +12,23 @@ def index(request):
     return render(request, "sayit/index.html", context=None)
 
 
+def get_hand(quantity):
+    with open('keys.keys') as f:
+        key = f.readline()
+    image_urls = []
+    for i in range(quantity):
+        response = requests.get("https://api.unsplash.com/photos/random?client_id=" + key.strip()).json()
+        image_urls.append(response['urls']['raw'])
+
+    return image_urls
+
+
 def play(request, player_id):
     player = get_object_or_404(Player, pk=player_id)
     players = Player.objects.filter()
-
+    hand_of_cards = get_hand(6)
     context = {
+        "hand": hand_of_cards,
         "player": player,
         "players": players,
     }
